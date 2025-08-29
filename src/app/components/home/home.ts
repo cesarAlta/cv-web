f10b339f2ee7e4e7452e19ad6bbc69f2a206e0af
cesarAlta <rces0365@gmail.com>
2025-08-29 16:57:23 -0300

    ajustes

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
-import { Profile } from "../profile/profile";
-import { Nav } from "../nav/nav";
+import { Profile } from '../profile/profile';
+import { Nav } from '../nav/nav';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { featherAirplay } from '@ng-icons/feather-icons';
+import {
+  diAngularOriginal,
+  diBootstrapOriginal,
+  diCss3Original,
+  diDockerOriginal,
+  diGithubOriginal,
+  diHtml5Original,
+  diJavaOriginal,
+  diJavascriptOriginal,
+  diMysqlOriginal,
+  diPostmanOriginal,
+  diSpringOriginal,
+  diTypescriptOriginal,
+} from '@ng-icons/devicon/original'; // Removed: not exported
 
 @Component({
   selector: 'app-home',
-  imports: [Profile, Nav],
+  imports: [Profile, Nav, NgIcon],
   templateUrl: './home.html',
-  styleUrl: './home.css'
-})
-export class Home {
+  styleUrl: './home.css',
+  viewProviders: [
+    provideIcons({
+      featherAirplay,
+      diJavascriptOriginal,
+      diTypescriptOriginal,
+      diCss3Original,
+      diHtml5Original,
+      diSpringOriginal,
+      diBootstrapOriginal, 
+      diAngularOriginal,
+      diJavaOriginal,
+      diMysqlOriginal,diPostmanOriginal,
+      diGithubOriginal,diDockerOriginal
 
-}
+
+    }),
+  ],
+  standalone: true,
+})
+export class Home {}
